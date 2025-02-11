146fb657309b935ae8303bf369f6852da1038a27
Anton Johansson <anjo@rev.ng>
2025-02-12 00:26:00 +0100

    Update install script
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- install-xqci.sh
+++ install-xqci.sh
@@ -6,6 +6,8 @@ cp build/*.decode submodules/xqci/target/riscv/xqci/
 cp build/xqciu_tcg.c submodules/xqci/target/riscv/xqci/
 cp build/xqciu_tcg.h submodules/xqci/target/riscv/xqci/
 cp build/xqciu_trans.c.inc submodules/xqci/target/riscv/xqci/
-cp build/xqciu-decode-extra-16.c.inc submodules/xqci/target/riscv/xqci/
-cp build/xqciu-decode-extra-32.c.inc submodules/xqci/target/riscv/xqci/
-cp build/xqciu-decode-extra-48.c.inc submodules/xqci/target/riscv/xqci/
+cp build/riscv-xqci.c submodules/xqci/disas/
+cp build/riscv-xqci.h submodules/xqci/disas/
+cp build/riscv-xqci-16-decode.c.inc submodules/xqci/disas/
+cp build/riscv-xqci-32-decode.c.inc submodules/xqci/disas/
+cp build/riscv-xqci-48-decode.c.inc submodules/xqci/disas/
